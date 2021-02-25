1aeaebc03bb09f89f562faf68fe8ecbb1dc2116d
GaDii Jian <ggd747@gmail.com>
2021-02-25 10:58:52 +0800

    Change brew installation

--- provision.sh
+++ provision.sh
@@ -3,7 +3,7 @@
 which -s brew
 if [[ $? != 0 ]] ; then
   echo "Installing Homebrew ..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   echo "Homebrew already installed, updating ..."
   brew update
